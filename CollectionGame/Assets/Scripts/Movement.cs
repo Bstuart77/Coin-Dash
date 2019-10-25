0d0e680ff53542f187d1b0f30c92961e6f8593c0
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2019-10-24 22:12:28 -0400

    Movement and collision with walls
    Player can now move, and collide with walls. Made audio file for future testing.

---
+++ CollectionGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 200f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        rb.MovePosition(transform.position + movement);
+    }
+}
