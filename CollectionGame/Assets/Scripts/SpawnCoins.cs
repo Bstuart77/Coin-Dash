fe69eddd22efbdf890cfacd0f88ed8146ec7dd8d
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2019-10-29 19:02:11 -0400

    Added random spawner for coin, and animation for coin

---
+++ CollectionGame/Assets/Scripts/SpawnCoins.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoins : MonoBehaviour
+{
+       public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int coinCount;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CoinSpawn());  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            other.GetComponent<Movement>().points++;
+            Destroy(gameObject);
+        }
+        
+    }
+  
+    IEnumerator CoinSpawn()
+    {
+        while (coinCount < 10)
+        {
+            xPos = Random.Range(-20, 20);
+            zPos = Random.Range(-20, 20);
+            Instantiate(theEnemy, new Vector3(xPos,1.61f,zPos), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.1f);
+            coinCount += 1;
+        }
+    }
+}
---
+++ CollectionGame/Assets/Scripts/coinRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinRotation : MonoBehaviour
+{
+  
+    
+   
+    void Update()
+    {
+        transform.Rotate(90f * Time.deltaTime, 0, 0);
+    }
+}
