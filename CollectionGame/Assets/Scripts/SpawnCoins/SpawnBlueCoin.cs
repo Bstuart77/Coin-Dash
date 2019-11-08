01ed47af44e9db89f01882c4f796702aebe22f2d
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2019-11-07 19:35:36 -0500

    Added a blue coin worth 10 points

---
+++ CollectionGame/Assets/Scripts/SpawnCoins/SpawnBlueCoin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlueCoin : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int coinCount = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BlueSpawnCoin());
+    }
+
+    IEnumerator BlueSpawnCoin()
+    {
+        while (coinCount < 10)
+        {
+            xPos = Random.Range(-20, 20);
+            zPos = Random.Range(-20, 20);
+            Instantiate(theEnemy, new Vector3(xPos, 1.61f, zPos), Quaternion.identity);
+
+            yield return new WaitForSeconds(1.5f);
+            coinCount++;
+        }
+    }
+}
