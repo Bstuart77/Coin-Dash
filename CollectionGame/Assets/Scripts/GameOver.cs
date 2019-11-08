01d323d9399db492dc8cc3c35cb8aa598150c2cd
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2019-11-07 20:19:58 -0500

    Created a Countdown Timer from 60 seconds

---
+++ CollectionGame/Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOver : MonoBehaviour
+{
+    public float timeLeft = 60.0f;
+    public Text startText; 
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        startText.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            //fix to make it transfer to another screen/leaderboard
+            Destroy(gameObject);
+        }
+    }
+}
