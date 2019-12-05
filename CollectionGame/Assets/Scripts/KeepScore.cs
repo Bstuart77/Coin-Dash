940ea60634a85b7fc2421053c61a6d216ea73d8a
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2019-12-05 12:47:09 -0500

    Made score font bigger

--- CollectionGame/Assets/Scripts/KeepScore.cs
+++ CollectionGame/Assets/Scripts/KeepScore.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class KeepScore : MonoBehaviour
 {
+    
     public static int Score = 0;
-
+    private GUIStyle font;
+    public void Start()
+    {
+        font = new GUIStyle();
+        font.fontSize = 36;
+    }
     private void OnGUI()
     {
-            GUI.Box(new Rect(200, 200, 200, 200), Score.ToString());
+            GUI.Box(new Rect(200, 200, 200, 200), Score.ToString(),font);
     }
 }
