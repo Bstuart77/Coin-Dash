c27f4e49113d6b5167e99e14974d2b3c84e7f012
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2019-12-12 15:43:18 -0500

    Added InputField button

---
+++ CollectionGame/Assets/Scripts/GetName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetName : MonoBehaviour
+{
+    public static string name;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void SaveName()
+    {   
+        name = inputField.GetComponent<Text>().text;
+        print(textDisplay.GetComponent<Text>().text = " Welcome " + name + " to the game");
+    }
+
+}
