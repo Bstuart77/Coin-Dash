fe5fc80418234cf9e9a4c063f967fb5654f956c3
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-01 02:16:15 -0500

    Script to randomely spawn, taking the rotation of the original object

---
+++ CollectionGame/Assets/Scripts/SpawnCoins/SpawnTimeExtenstion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTimeExtenstion : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        StartCoroutine(TimeExtenstion());
+    }
+
+    IEnumerator TimeExtenstion()
+    {
+
+        while (Timer.timeLeft > 0)
+        {
+            xPos = Random.Range(-20, 20);
+            zPos = Random.Range(-20, 20);
+            Instantiate(theEnemy, new Vector3(xPos, 1.61f, zPos), theEnemy.transform.rotation);
+
+            yield return new WaitForSeconds(10f);
+
+    
+        }
+    }
+}
