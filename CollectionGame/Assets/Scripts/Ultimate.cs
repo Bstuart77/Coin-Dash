2820ed152844547360c28ba7477435a4d5d08ac4
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-07 01:25:10 -0500

    added a ult script, which has particles and resets when space is pressed

---
+++ CollectionGame/Assets/Scripts/Ultimate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ultimate : MonoBehaviour
+{
+    public Text txt;
+    public static int ult;
+    public ParticleSystem particles;
+
+
+    private void Start()
+    {
+        float axis = Input.GetAxis("Horizontal");
+    }
+    private void Update()
+    {
+        txt.text = ult.ToString();
+
+        if (ult >= 100)
+        {
+            ult = 100;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && (ult==100)){
+    
+            ult = 0;
+        }
+    }
+}
