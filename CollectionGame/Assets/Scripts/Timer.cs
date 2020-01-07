a44108c2ee08f0379e86f980cc874bf5f3224fa7
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-06 23:56:03 -0500

    Revert "lowered timer and swithced .loadScene for testing pruposes"
    This reverts commit 2e48f497636275d758eac4ade167f9f37c446907.

--- CollectionGame/Assets/Scripts/Timer.cs
+++ CollectionGame/Assets/Scripts/Timer.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
-    public static float timeLeft = 5.0f;
+    public static float timeLeft = 60.0f;
     public Text startText;
 
 
@@ -15,7 +15,7 @@ public class Timer : MonoBehaviour
         startText.text = (timeLeft).ToString("0");
         if (timeLeft < 0.0)
         {
-            SceneManager.LoadScene("LeaderBoard");
+            SceneManager.LoadScene("EndGame");
             timeLeft = 60.0f;
         }
     }
