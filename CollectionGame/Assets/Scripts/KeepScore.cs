c7028a2e4a734efa4a1f60bc3a6508d578bb6ebe
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-11 04:58:52 -0500

    removed unNeeded code

--- CollectionGame/Assets/Scripts/KeepScore.cs
+++ CollectionGame/Assets/Scripts/KeepScore.cs
@@ -7,12 +7,7 @@ public class KeepScore : MonoBehaviour
 {
     public Text scoreText;
     public static int Score = 0;
-    private GUIStyle font;
-    public void Start()
-    {
-        font = new GUIStyle();
-        font.fontSize = 1;
-    }
+ 
     private void Update()
     {
         scoreText.text = Score.ToString();
--- CollectionGame/Assets/Scripts/Movement.cs
+++ CollectionGame/Assets/Scripts/Movement.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 200f;
+    public float moveSpeed = 20f;
     private Rigidbody rb;
-    public int points = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * (moveSpeed * Time.deltaTime), 0f, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
