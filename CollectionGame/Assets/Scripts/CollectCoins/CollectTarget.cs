29bf07f45a57097ba637ae6e6a26a2b19bf79e75
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-15 03:40:49 -0500

    Revert "Revert "added script that when gameObject is clicked adds 5 points""
    This reverts commit e8cfae5f19d03f70125ebd77ae6d4b86f422d7dd.

---
+++ CollectionGame/Assets/Scripts/CollectCoins/CollectTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectTarget : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        KeepScore.Score += 5;
+        Destroy(gameObject);
+    }
+    private void Update()
+    {
+        if(gameObject.name == "target(Clone)")
+        {
+            Destroy(gameObject, 1f);
+        }
+    }
+}
