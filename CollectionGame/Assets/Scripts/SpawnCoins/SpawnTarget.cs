9f44500df958435207a02217686727b0504dfb1d
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-15 03:41:47 -0500

    created script to spawn target coin

---
+++ CollectionGame/Assets/Scripts/SpawnCoins/SpawnTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTarget : MonoBehaviour
+{
+    public GameObject enemy;
+    private int xPos;
+    private int zPos;
+
+    void Start()
+    {
+       StartCoroutine(spawnTarget());
+    }
+
+    IEnumerator spawnTarget()
+    {
+        while(Timer.timeLeft > 0)
+        {
+            xPos = Random.Range(-20, 20);
+            zPos = Random.Range(-20, 20);
+            Instantiate(enemy, new Vector3(xPos, 1.6f, zPos),enemy.transform.rotation);
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
