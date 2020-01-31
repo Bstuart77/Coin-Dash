37a81a1781219f398593a2ab0bd21aeb4e0f1429
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-01-31 15:23:05 -0500

    Created a Pause screen that can quit game, load main menu, resume game or restart game

---
+++ CollectionGame/Assets/Scripts/OnScreen/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else 
+            {
+                Pause();
+            }
+        }
+    }
+   public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+   void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Welcome");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();     
+    }
+    public void Resart()
+    {
+        SceneManager.LoadScene("Game");
+        KeepScore.Score = 0;
+        Timer.timeLeft = 60f;
+        Time.timeScale = 1f;
+    }
+}
