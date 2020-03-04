449423d552544ff92a53b2e0039d1140042177ba
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-03-03 19:08:36 -0500

    scirpt that updates the audio with audio slider

---
+++ CollectionGame/Assets/Scripts/Sound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    public static AudioSource AudioSrc;
+
+    private float AudioVolume = 1f;
+
+    void Start()
+    {
+        AudioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        AudioSrc.volume = AudioVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        AudioVolume = vol;
+    }
+
+}
