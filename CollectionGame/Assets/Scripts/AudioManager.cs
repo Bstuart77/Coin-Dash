ff6d982ab7f3af889754fae55ba70dfc5e5034aa
Brian Stuart <42984201+Bstuart77@users.noreply.github.com>
2020-03-16 20:31:25 -0400

    script that changes volume based on slider and saves it

---
+++ CollectionGame/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource music;
+    public Slider musicSlider;
+
+    private void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+    void Update()
+    {
+        music.volume = musicSlider.value;
+    }
+
+    public void volumePrefs()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", music.volume);
+    }
+
+    
+}
